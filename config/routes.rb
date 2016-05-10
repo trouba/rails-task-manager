e34e3ac18e6b350f99795cce7a661bd999661448
Nicolas Rouault <trouba@gmail.com>
2016-05-10 16:43:51 +0200

    task manager in rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # get 'tasks/index'
+
+  # get 'tasks/show'
+
+  # get 'tasks/new'
+
+  # get 'tasks/create'
+
+  # get 'tasks/edit'
+
+  # get 'tasks/update'
+
+  # get 'tasks/destroy'
+
+  resources :tasks
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
